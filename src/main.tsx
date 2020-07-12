b099cfda74e65ae758fff6c86db6051679a1f48b
Alexandre <amoutonbrady@gmail.com>
2020-07-13 00:01:32 +0200

    :bug: Prevent warning of computation
    Need to investigate this deeper, but for now that'll do

--- src/main.tsx
+++ src/main.tsx
@@ -1,77 +1,64 @@
-import match from "regexparam";
 import {
   createContext,
   createState,
   Component,
   useContext,
   Show,
-  createRoot,
 } from "solid-js";
+
+import match from "regexparam";
 import { createBrowserHistory } from "history";
 
-const router = createRoot(() => {
-  const browser = createBrowserHistory();
-  const currentRoute = browser.location;
-  const [routerState, setRouterState] = createState({ currentRoute });
+const browser = createBrowserHistory();
+const currentRoute = browser.location;
+const [routerState, setRouterState] = createState({ currentRoute });
 
-  const store = [
-    routerState,
-    {
-      push(path: string) {
-        browser.push(path);
-      },
+const store = [
+  routerState,
+  {
+    push(path: string) {
+      browser.push(path);
     },
-  ] as const;
+  },
+] as const;
 
-  const RouterContext = createContext(store);
+const RouterContext = createContext(store);
 
-  function useRouter() {
-    return useContext(RouterContext);
-  }
+export function useRouter() {
+  return useContext(RouterContext);
+}
 
-  const Router: Component = (props) => {
-    browser.listen(({ location }) => {
-      setRouterState("currentRoute", location);
-    });
+export const Router: Component = (props) => {
+  browser.listen(({ location }) => {
+    setRouterState("currentRoute", location);
+  });
 
-    return (
-      <RouterContext.Provider value={store}>
-        {props.children}
-      </RouterContext.Provider>
-    );
+  return () =>
+    RouterContext.Provider({ value: store, children: props.children });
+};
+
+export const Route: Component<{ path: string }> = (props) => {
+  const [router] = useRouter();
+  const routeMatcher = match(props.path).pattern;
+  const isActiveRoute = () => !!routeMatcher.test(router.currentRoute.pathname);
+
+  return () => (isActiveRoute() ? props.children : false);
+};
+
+export const Link: Component<{ path: string }> = ({ path, children }) => {
+  const [_, { push }] = useRouter();
+  const handleClick = () => push(path);
+
+  return (
+    <a href={path} onClick={prevent(handleClick)}>
+      {children}
+    </a>
+  );
+};
+
+function prevent(fn: (event: Event) => any) {
+  return (event: Event) => {
+    event.preventDefault();
+    fn(event);
   };
-
-  const Route: Component<{ path: string }> = (props) => {
-    const [router] = useRouter();
-    const routeMatcher = match(props.path).pattern;
-    const isActiveRoute = () =>
-      !!routeMatcher.test(router.currentRoute.pathname);
-
-    return <Show when={isActiveRoute()}>{props.children}</Show>;
-  };
-
-  const Link: Component<{ path: string }> = (props) => {
-    const [_, { push }] = useRouter();
-    const handleClick = () => push(props.path);
-
-    return (
-      <a href={props.path} onClick={prevent(handleClick)}>
-        {props.children}
-      </a>
-    );
-  };
-
-  function prevent(fn: (event: Event) => any) {
-    return (event: Event) => {
-      event.preventDefault();
-      fn(event);
-    };
-  }
-
-  return { useRouter, Router, Route, Link };
-});
-
-export const useRouter = router.useRouter;
-export const Router = router.Router;
-export const Route = router.Route;
-export const Link = router.Link;
+}
